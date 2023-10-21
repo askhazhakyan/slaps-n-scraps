ca8b34ff57a8500d04f3aa243cc9f70ae8593812
askhazhakyan <alexkhazhakyan@ymail.com>
2023-10-20 20:22:51 -0700

    Created React Router w/ Page Routing. Also created multiple components.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Landing from './components/Landing/Landing';
+import Slaps from'./components/Slaps/Slaps';
+import './App.css'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<span>{<Landing/>}</span>
+  },
+  {
+    path:"/slaps",
+    element:<span>{<Slaps/>}</span>
+  },
+])
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
 }
